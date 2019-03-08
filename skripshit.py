8d098815247bcee9b3ecdb727c8d1dcda7aa47be
bagas <bagasmuhammad1999@gmail.com>
2019-03-08 18:13:35 +0700

    nambahin skripshit salah

---
+++ skripshit.py
@@ -0,0 +1,20 @@
+from xml.dom import minidom
+import os
+
+datadir = '/media/chiken-eater/PELAJARAN/Data Skripsi/frame/foto plat/'
+files = os.listdir(datadir)
+
+for file in files:
+    if file.endswith('.xml'):
+        print(file)
+        skrishit = minidom.parse(os.path.join(datadir, file))
+        xmin = int(skrishit.getElementsByTagName('xmin')[0].firstChild.data)
+        ymin = int(skrishit.getElementsByTagName('ymin')[0].firstChild.data)
+        xmax = int(skrishit.getElementsByTagName('xmax')[0].firstChild.data)
+        ymax = int(skrishit.getElementsByTagName('ymax')[0].firstChild.data)
+        print(xmin)
+        print(ymin)
+        print(xmax)
+        print(ymax)
+
+namabahini isis something
